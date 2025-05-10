4f4d206e4470ade3fd3f06015d9c00f185392173
Colin Goodall <cgoodall@GoodRiver.ca>
2025-05-10 09:43:05 -0400

    Changed for LocalTask1

--- print_args.c
+++ print_args.c
@@ -1,11 +1,18 @@
 /*
 *  print_args.c
+*
+*  LocalTask1.
+*     Added a line at start
+*
+*
+*
 */
 
 #include <stdio.h>
 
 int  main( int argC, const char * argV[] )
 {
+	printf( "---------------------------------------------\n" );
 	for ( int nIndex = 0; nIndex < argC; nIndex++ )
 	{
 		printf( "ARG[%d] -->%s<--\n", nIndex, argV[nIndex] );
